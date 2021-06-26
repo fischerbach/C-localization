b8d3e5efa66a9c283f94137f9b14196bd390830e
fischerbach <rafalrybnik@yahoo.com>
2021-06-26 16:29:45 +0200

    terminal app translation

--- terminal-app/do_nothing.c
+++ terminal-app/do_nothing.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(String) gettext(String)
 
 int main(int argc, char *argv[]) {
     int opt;
@@ -9,13 +13,17 @@ int main(int argc, char *argv[]) {
     int i_flag = 0;
     int g_flag = 0;
 
+    bindtextdomain ("base", "./locales/");
+    textdomain ("base");
+
+
     while ( (opt = getopt(argc, argv, "hi:g:")) != -1 ) {
         switch(opt) {
             case 'h':
-                printf("\033[1;37mThis application does absolutely nothing.\n\n");
-                printf("Available options:\n");
-                printf("-i <file>\tDoes nothing with specified file. \n");
-                printf("-g <file>\tDoes nothing with specified file, but in \033[0;32mgreen color\033[0;37m.\n");
+                printf(_("\033[1;37mThis application does absolutely nothing.\n\n"));
+                printf(_("Available options:\n"));
+                printf(_("-i <file>\tDoes nothing with specified file. \n"));
+                printf(_("-g <file>\tDoes nothing with specified file, but in \033[0;32mgreen color\033[0;37m.\n"));
 
                 break;
             case 'i':
@@ -27,28 +35,28 @@ int main(int argc, char *argv[]) {
                 g_flag = 1;
                 break;
             default:
-                printf("Use -h to display help.\n");
+                printf(_("Use -h to display help.\n"));
                 return 0;
         }
     }
 
     if (!i_flag && !g_flag) {
-        printf("Use -h to display help.\n");
+        printf(_("Use -h to display help.\n"));
     }
 
     if ((i_flag || g_flag) && !filename) {
-        printf("You must specify a destination file name.\n");
+        printf(_("You must specify a destination file name.\n"));
         return 1;
     }
 
     if (i_flag) {
-        printf("Doing nothing with %s file...\n", filename);
-        printf("Done.\n");
+        printf(_("Doing nothing with %s file...\n"), filename);
+        printf(_("Done.\n"));
     }
 
     if (g_flag) {
-        printf("\033[0;32mDoing nothing with %s file...\n", filename);
-        printf("Done.\n");
+        printf(_("\033[0;32mDoing nothing with %s file...\n"), filename);
+        printf(_("Done.\n"));
     }
 
     return 0;
