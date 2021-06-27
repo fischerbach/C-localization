921c462fa18785ab25e2bd14d4e20d6d2463bb64
fischerbach <rafalrybnik@yahoo.com>
2021-06-27 12:29:45 +0200

    locales build scripts

---
+++ gui-app/gen_locale_files.sh
@@ -0,0 +1,3 @@
+xgettext  -k_ -o locales/base.pot --language=C do_nothing_gui.c
+msginit --input=locales/base.pot --locale=pl --output=locales/pl/base.po
+
---
+++ gui-app/gen_mo_files.sh
@@ -0,0 +1,1 @@
+msgfmt --output-file=locales/pl/LC_MESSAGES/base.mo locales/pl/base.po
